b8474fa7618eabd7606d2a1f2bf40f9cb75a773d
Neri <simonsystem2010.neri@gmail.com>
2025-05-12 17:01:38 -0300

    Le puse tu nombre y apellido.

--- codigo.py
+++ codigo.py
@@ -1,5 +1,4 @@
-nombre = "luquitas"
-apellido = "grisales"
-
+nombre = "Alejandro" #Cambie por tu nombre
+apellido = "Grisales"#Cambie por tu apellido
 saludo = "Hola " + nombre + " " + apellido + "¿como estas?"
 print(saludo)
